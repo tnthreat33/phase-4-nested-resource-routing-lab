95790ce75cf4c8460891f3bfb292fb2933abffac
Rachel Hamby <tnthreat33@gmail.com>
2023-05-24 20:40:36 -0400

    commit tree 9b7cbb84a3f95c70a3dbd325a765431162a13224

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,28 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items 
+    else
     items = Item.all
-    render json: items, include: :user
+    
   end
+  render json: items, include: :user
+end
 
+def show 
+  item = Item.find(params[:id])
+  render json: item 
+end 
+
+def create 
+  user = User.find(params[:user_id])
+  item = user.items.create(params.permit(:name, :description, :price))
+render json: item, status: :created 
+end 
+
+def render_not_found_response(exception)
+  render json: { error: "#{exception.model} not found" }, status: :not_found
+end
 end
